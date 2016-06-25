49d17f052d3837b91b5fc46cdad44feb59a2f8f3
Anabelle Handdoek <ana@gathacol.net>
2016-06-25 16:19:01 -0500

    primera version animacion e icono mano

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,31 @@
 $( document ).ready( function($){
-	console.log('\'Allo \'Allo!');
+	var animado = 1;
+
+	$('.persona').hide();
+	$('.boton').css('opacity', 0);
+	$('.persona:first-child').show();
+	setInterval( function(){ animar() }, 150 );
+	$('.personas img').on('click', function(){ toggleAnimar() });
+
+	function animar(){
+		if( animado == 1 ){
+			if( $('.persona:visible').next().length != 0 ){
+				$('.persona:visible').hide().next().show();
+			}else{
+				$('.persona:visible').hide();
+				$('.persona:first-child').show();
+			}
+		}
+	}
+
+	function toggleAnimar(){
+		if( animado == 1 ){
+			animado = 0;
+			$('.boton').css('opacity', 1);
+		}else{
+			animado = 1;
+			$('.boton').css('opacity', 0);
+		}
+	}
+
 });
